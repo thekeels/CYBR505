f1a78460491751d24e29f2e05d780cf978f2da6d
kswanson <kylecol@msn.com>
2017-05-30 16:00:52 -0700

    Added change to identify presence of complex root for lab5

--- CYBR505/Lab5.c
+++ CYBR505/Lab5.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	double a, b, c, root1, root2;
+	double imag;
 	printf("Enter the coefficients: \n");
 	scanf_s("%lf", &a);
 	scanf_s("%lf", &b);
@@ -10,8 +11,14 @@ int main(void)
 	root1 = (-b + sqrt(pow(b, 2) - 4 * a*c)) / (2 * a);
 	root2 = (-b - sqrt(pow(b, 2) - 4 * a*c)) / (2 * a);
 	printf("The two roots of the function, ax^2 +bx +c =0, are:\n");
-	printf("x1= %lf\n", root1);
-	printf("x1= %lf\n", root2);
+	if ((pow(b, 2) - 4 * a*c)<0) {
+		printf("...Sorry, this quadratic equation has complex roots!\n");
+	}
+	else {
+		printf("x1= %lf\n", root1);
+		printf("x1= %lf\n", root2);
+	}
+
 	getchar();
 	getchar();
 	return 0;
