95e5ee33cd2f1ae9b94bb61d6e9c0657f4bdb287
kswanson <kswanson@ENGR-L209-24>
2017-05-30 19:31:57 -0700

    add labs to main

--- CYBR505/LabMain.c
+++ CYBR505/LabMain.c
@@ -3,7 +3,7 @@ int main(void)
 {
 	int labnumber;
 	int endlabs = 1;
-	int Lab1(), Lab2(), Lab3();
+	int Lab1(), Lab2(), Lab3(), Lab4(), Lab5(), Lab6();
 	do {
 		system("cls");
 		printf("Enter the lab number you want to run (0 to exit): ");
@@ -18,6 +18,18 @@ int main(void)
 			Lab2();
 		}
 		else if (labnumber == 3)
+		{
+			Lab3();
+		}
+		else if (labnumber == 4)
+		{
+			Lab3();
+		}
+		else if (labnumber == 5)
+		{
+			Lab3();
+		}
+		else if (labnumber == 6)
 		{
 			Lab3();
 		}
