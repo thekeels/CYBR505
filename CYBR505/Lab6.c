a5d9b2512eed7ba93d3780a9f85d3b680b079ed1
kswanson <kswanson@ENGR-L209-24>
2017-05-30 19:07:32 -0700

    Added lab6.c

---
+++ CYBR505/Lab6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	int x;
+	printf("Please enter a number: \n");
+	scanf_s("%d", &x);
+	if (x % 2 == 0)
+	{
+		printf("This number is even.\n");
+	}
+	else
+	{
+		printf("This number is odd.\n");
+	}
+	getchar();
+	getchar();
+	return 0;
+
+}
