bd03d9ae9ab5cd3dc182a07d849458f282b046d5
kswanson <kylecol@msn.com>
2017-06-02 14:04:40 -0700

    finished lab 9

---
+++ CYBR505/Lab9.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	int i, j;
+	double score_sum, score, average;
+	
+	for (i = 1; i < 21; i++)
+	{
+		score_sum = 0;
+		printf("Enter student %d grade\n", i);
+		for (j = 0; j < 4; j++)
+		{
+
+			scanf_s("%lf", &score);
+			score_sum += score;
+		}
+		average = score_sum / 4;
+		printf("The average grade for student %i is %.4g\n", i, average);
+	}
+	getchar();
+	getchar();
+	return 0;
+}
