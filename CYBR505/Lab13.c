cc2448910819e64e272119647fd4a36daf2b8967
kswanson <kswanson@ENGR-L209-24>
2017-06-13 19:21:35 -0700

    Added Lab13

---
+++ CYBR505/Lab13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+float AverageTwo(int num1, int num2);
+
+int main()
+{
+	int score1, score2;
+	float ave;
+	printf("Please enter the 2 numbers to average: ");
+	scanf_s("%d %d", &score1, &score2);
+	ave = AverageTwo(score1, score2);
+	printf("\nThe average is: %.2f", ave);
+	getchar();
+	getchar();
+	return 0;
+}
+// AverageTwo is a function to take 2 integers and compute the float average.
+// Inputs: 2 integers
+// Outputs: 1 float
+float AverageTwo(int num1, int num2)
+{
+	float average = (num1 + num2) / 2;
+	return average;
+}
