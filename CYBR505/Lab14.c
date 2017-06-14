963ddfc1179e3d60ccd5f06b642b93ff4f37813e
kswanson <kswanson@ENGR-L209-24>
2017-06-13 19:52:07 -0700

    Added Lab14

---
+++ CYBR505/Lab14.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int Fact(int n);
+
+int main()
+{
+	int n, output;
+	int sentinel = -1000;
+
+	printf("Please enter n\n");
+	scanf_s("%d", &n);
+	while (sentinel == -1000) {
+		if (n >= 0 && n <= 10) {
+			output = Fact(n);
+			sentinel = 0;
+		}
+		else
+		{
+			printf("This number is outside the range.\n");
+			printf("Please enter n\n");
+			scanf_s("%d", &n);
+		}
+	}
+	
+	printf("%d! is %d", n, output);
+	getchar();
+	getchar();
+	return(0);
+}
+// Fact caclulates the factorial of a number i.e. n(!) = 1*2*3*4...*(n-1)*n
+// Input: integer to calculate factorial of
+// Output: integer result
+int Fact(int n)
+{
+	int fact = 1;
+	for (int i = 1; i <= n; i++)
+		fact *= i;
+	return fact;
+}
