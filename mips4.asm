aa1380369bc4bc4cfcd0af2d699bc7523987a6e7
kswanson <kylecol@msn.com>
2017-07-06 23:15:37 -0700

    Add extra lab mips4.asm

--- mips4.asm
+++ mips4.asm
@@ -1,89 +1,128 @@
 .data
 frame: .asciiz "\n=============================================\n"
 inputRequest: .asciiz "How many positive numbers that are divisible by 6 do you want to add?\n"
-enterNumber: .asciiz "\n\nEnter a number: "
-errorPrint: .asciiz "\n==>****ERROR: "
-errorNotPositive: .asciiz " is not a positive number. Enter another number.\n"
-errorNotInRange: .asciiz " is not in the range of 1 to 100. Enter another number.\n"
+enterNumber: .asciiz "\nEnter a number: "
+errorPrint: .asciiz "\n****ERROR: "
+errorNegative: .asciiz " is not a positive number. Enter another number.\n"
+errorOutOfRange: .asciiz " is not in the range of 1 to 100. Enter another number.\n"
 errorNotDivisible: .asciiz " is not divisible by 6. Enter another number.\n"
 isDivisible: .asciiz " is divisible by 6\n"
 result: .asciiz "\nThe Sum of the positive numbers between 1 and 100 that are divisible by 6, is: "
-newline: .asciiz "\n"
 
 .text
 
 main:
+# Print header and input prompt
 li $v0, 4
 la $a0, frame
 syscall
 la $a0, inputRequest
 syscall
-# Set counter ($s2) to 0, max ($s3) to 100
-li $s2, 0
-li $s3, 100
-# Load 6 into $s1
+# Set $s1 to 6 (for division), counter ($s2) to 0, sum accumulator ($s3) to 0
 li $s1, 6
+li $s2, 0
+li $s3, 0
 # Get number of inputs from user
 li $v0, 5
 syscall
 # Save numbers to add in $s0
 add $s0,$v0,$0
-
+# Loop here to check the counter
 checkCounter:
 beq $s2, $s0, end
-
+# Prompt for a number
 li $v0, 4
-
 la $a0, enterNumber
 syscall
+# Loop here after errors to retrieve a new number
+enterNumberAgain:
 # Read a number
 li $v0, 5
 syscall
-
-# Place number in $t0
+# Place read number in ($t0)
 add $t0,$v0,$0
-
-# Divide number ($t0) by 6
+# Check if number is negative or out of range
+blt $t0, $0, errorNegativeSpot 
+bgt $t0, 100, errorOutOfRangeSpot
+ble $t0, $0, errorOutOfRangeSpot
+# Divide number ($t0) by 6 ($s1)
 div $t0,$s1
+# Place remainde into $t1
 mfhi $t1
-# Check if remainder is 0
+# Check if remainder is 0 (if number is divisble by 6)
 beq $t1, $0, isDivisibleSpot
-blt $t0, $0, errorNegative
-bgt $t0, $s3, errorNotInRange
-j checkCounter
-
+# If not divisible by 6, read a new number
+j errorNotDivisibleSpot
 
 isDivisibleSpot:
+# Print the number
 li $v0, 1
 add $a0, $t0, $0
 syscall
-
+# Display that the number is divisible by 6
 li $v0, 4
 la $a0, isDivisible
 syscall
-
-add $s1, $s1, $t0
+# Increment counter and add to the sum accumulator
+add $s3, $s3, $t0
 addi $s2,$s2,1
+# Check if number of valid inputs has been reached
 j checkCounter
 
-errorNegative:
+errorNegativeSpot:
+# Print error
 li $v0, 4
-la $a0, errorNotPositive
+la $a0, errorPrint
 syscall
-j checkCounter
-
-errorOutOfRange:
+# Print the number
+li $v0, 1
+add $a0, $t0, $0
+syscall
+# Print the error message
 li $v0, 4
-la $a0, errorNotInRange
+la $a0, errorNegative
 syscall
-j checkCounter
+# Read a new number
+j enterNumberAgain
 
+errorOutOfRangeSpot:
+# Print error
+li $v0, 4
+la $a0, errorPrint
+syscall
+# Print the number
+li $v0, 1
+add $a0, $t0, $0
+syscall
+# Print the error message
+li $v0, 4
+la $a0, errorOutOfRange
+syscall
+# Read a new number
+j enterNumberAgain
+
+errorNotDivisibleSpot:
+# Print error
+li $v0, 4
+la $a0, errorPrint
+syscall
+# Print the number
+li $v0, 1
+add $a0, $t0, $0
+syscall
+# Print the error message
+li $v0, 4
+la $a0, errorNotDivisible
+syscall
+# Read a new number
+j enterNumberAgain
 
 end:
+# Print the result
 li $v0, 4
 la $a0, result
 syscall
 
 li $v0, 1
-add $a0, $s1, $0
+add $a0, $s3, $0
 syscall
